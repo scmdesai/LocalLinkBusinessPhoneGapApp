0a4135261cf81e83bc91607302e610b01616dfce
scmdesai <scmdesai@gmail.com>
2016-02-28 22:15:32 -0600

    silly mistake

--- app.js
+++ app.js
@@ -66870,10 +66870,10 @@ Ext.define('Ext.picker.Picker', {
                             var form = this.up('contactform');
                             var record = form.getRecord();
                             var customerId = form.getRecord().get('customerId');
-                            form.getComponent('CustomerId').setValue(customerId);
-                            form.getComponent('Category').setValue(form.getRecord().get('category'));
+                            form.getComponent('customerId').setValue(customerId);
+                            form.getComponent('category').setValue(form.getRecord().get('category'));
                             //form.getComponent('businessName').setValue(form.getRecord().get('businessName'));
-                            form.getComponent('email').setValue(form.getRecord().get('emailAddress'));
+                            form.getComponent('emailAddress').setValue(form.getRecord().get('emailAddress'));
                             form.getComponent('city').setValue(form.getRecord().get('city'));
                             form.getComponent('state').setValue(form.getRecord().get('state'));
                             form.getComponent('zipcode').setValue(form.getRecord().get('zipcode'));
