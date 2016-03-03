8ce7020a0cbfefdc4ebad2315d51d31a307756de
scmdesai <scmdesai@gmail.com>
2016-03-03 17:06:14 -0600

    pictureURL

--- app.js
+++ app.js
@@ -66628,7 +66628,7 @@ Ext.define('Ext.picker.Picker', {
             var customerId = record.get('customerId');
             this.down('#nameTxt').setHtml(name);
             this.down('contactpic').setData(record.data);
-            console.log(record.get('pictureURL'));
+            console.log(record.pictureURL);
         }
     }
 }, 0, [
