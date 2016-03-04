0031b1e199de6e09c260ce2eb098a21ee48293ff
scmdesai <scmdesai@gmail.com>
2016-03-03 20:55:05 -0600

    button

--- app.js
+++ app.js
@@ -66454,8 +66454,8 @@ Ext.define('Ext.picker.Picker', {
         styleHtmlContent: true,
         ui: '',
         tpl: [
-            '<div><img src="{pictureURL}"/></div>',
-            '<div><button type="button">Click Me </button></div>',
+            '<div><img src="{pictureURL}"/>',
+            '<button type="button">Click Me </button></div>',
             ''
         ],
         layout: {
