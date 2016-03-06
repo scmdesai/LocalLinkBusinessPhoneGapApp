e4f9f6d13f24292f2212fe4e61a18c7ef0b08ab9
scmdesai <scmdesai@gmail.com>
2016-03-05 22:40:05 -0600

    print record

--- app.js
+++ app.js
@@ -67354,7 +67354,7 @@ Ext.define('Ext.picker.Picker', {
         //var record = Ext.getStore('MyDealsStore').findRecord('customerId',customerId,0,true,false,false);
         //var rec = button.getParent().getParent().getDealPicture();
         var record = button.getParent().getParent().getData();
-        console.log(record.dealsPictureURL);
+        console.log(record);
         window.plugins.socialsharing.share('Hi!Check out the latest deal from ' + record.businessName, null, null, record.dealPictureURL);
     },
     onManageDealsTap: function(button, e, eOpts) {
