39b6d46a5bf8fb063be6327abbefc3dcb183ecc1
scmdesai <scmdesai@gmail.com>
2016-03-22 12:28:26 -0500

    btn hide/show

--- app.js
+++ app.js
@@ -66916,8 +66916,8 @@ Ext.define('Ext.picker.Picker', {
     },
     onEditButtonTap: function(button, e, eOpts) {
         var referrer = Ext.Viewport.getActiveItem();
-        var btn = Ext.getCmp('changePicButton');
-        btn.setVisible(true);
+        var btn = Ext.get('changePicButton');
+        btn.show();
         var form = this.getContactform();
         var info = this.getContactinfo().getRecord();
         form.referrer = referrer;
