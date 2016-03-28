216f73f0927b6ce9689cfaf76246f801bfedc6d6
scmdesai <scmdesai@gmail.com>
2016-03-28 15:10:05 -0500

    store load

--- app.js
+++ app.js
@@ -66025,10 +66025,17 @@ Ext.define('Ext.picker.Picker', {
             {
                 fn: 'onJsonstoreAddrecords',
                 event: 'addrecords'
+            },
+            {
+                fn: 'onJsonstoreRemoverecords',
+                event: 'removerecords'
             }
         ]
     },
     onJsonstoreAddrecords: function(store, records, eOpts) {
+        store.load();
+    },
+    onJsonstoreRemoverecords: function(store, records, indices, eOpts) {
         store.load();
     }
 }, 0, 0, 0, 0, 0, 0, [
@@ -66929,10 +66936,10 @@ Ext.define('Ext.picker.Picker', {
                             url: 'http://services.appsonmobile.com/deals/' + itemName,
                             success: function(form, action) {
                                 Ext.Msg.alert('Success', action.msg);
-                                //console.log(action.msg);
-                                var dealsStore = Ext.getStore('MyDealsStore');
-                                dealsStore.load();
                             },
+                            //console.log(action.msg);
+                            //var dealsStore = Ext.getStore('MyDealsStore');
+                            //dealsStore.load();
                             failure: function(form, action) {
                                 Ext.Msg.alert('Failure', action.msg);
                             }
@@ -67553,14 +67560,14 @@ Ext.define('Ext.picker.Picker', {
                                     xhr2: true,
                                     scope: this,
                                     success: function(form, action) {
-                                        Ext.getStore('MyDealsStore').load();
+                                        // Ext.getStore('MyDealsStore').load();
                                         Ext.Msg.alert('Success', action.msg);
                                         //console.log("Action Msg is : " +action.success);
                                         //Ext.Viewport.setActiveItem({xtype:'DealsPanel'});
                                         uForm.destroy();
                                     },
                                     failure: function(form, action) {
-                                        Ext.getStore('MyDealsStore').load();
+                                        //Ext.getStore('MyDealsStore').load();
                                         Ext.Msg.alert('Failure', action.msg);
                                         console.log("Action Msg is : " + action.msg);
                                         //Ext.Viewport.setActiveItem({xtype:'DealsPanel'});
