64bd4d58f0bd1dd568cd9ea4ba0df63f243b447d
scmdesai <scmdesai@gmail.com>
2016-04-13 12:02:11 -0500

    chart option

--- app.js
+++ app.js
@@ -66675,9 +66675,9 @@ Ext.define('Ext.picker.Picker', {
                                     var options = {
                                             'pieHole': 0.4,
                                             'pieSliceTextStyle': {
-                                                color: 'black',
-                                                height: '500'
-                                            }
+                                                color: 'black'
+                                            },
+                                            height: '500'
                                         };
                                     // Instantiate and draw our chart, passing in some options.
                                     var chart = new google.visualization.PieChart(document.getElementById('chart2'));
