1bfe1eb24e8cdc99f2bac0ce394348a3a2e8ce1a
scmdesai <scmdesai@gmail.com>
2016-04-14 10:37:58 -0500

    pic

--- app.js
+++ app.js
@@ -32414,6 +32414,220 @@ this);
 ], 0));
 
 /**
+ * This is a simple way to add an image of any size to your application and have it participate in the layout system
+ * like any other component. This component typically takes between 1 and 3 configurations - a {@link #src}, and
+ * optionally a {@link #height} and a {@link #width}:
+ *
+ *     @example miniphone
+ *     var img = Ext.create('Ext.Img', {
+ *         src: 'http://www.sencha.com/assets/images/sencha-avatar-64x64.png',
+ *         height: 64,
+ *         width: 64
+ *     });
+ *     Ext.Viewport.add(img);
+ *
+ * It's also easy to add an image into a panel or other container using its xtype:
+ *
+ *     @example miniphone
+ *     Ext.create('Ext.Panel', {
+ *         fullscreen: true,
+ *         layout: 'hbox',
+ *         items: [
+ *             {
+ *                 xtype: 'image',
+ *                 src: 'http://www.sencha.com/assets/images/sencha-avatar-64x64.png',
+ *                 flex: 1
+ *             },
+ *             {
+ *                 xtype: 'panel',
+ *                 flex: 2,
+ *                 html: 'Sencha Inc.<br/>1700 Seaport Boulevard Suite 120, Redwood City, CA'
+ *             }
+ *         ]
+ *     });
+ *
+ * Here we created a panel which contains an image (a profile picture in this case) and a text area to allow the user
+ * to enter profile information about themselves. In this case we used an {@link Ext.layout.HBox hbox layout} and
+ * flexed the image to take up one third of the width and the text area to take two thirds of the width. See the
+ * {@link Ext.layout.HBox hbox docs} for more information on flexing items.
+ */
+(Ext.cmd.derive('Ext.Img', Ext.Component, {
+    /**
+     * @event tap
+     * Fires whenever the component is tapped
+     * @param {Ext.Img} this The Image instance
+     * @param {Ext.EventObject} e The event object
+     */
+    /**
+     * @event load
+     * Fires when the image is loaded
+     * @param {Ext.Img} this The Image instance
+     * @param {Ext.EventObject} e The event object
+     */
+    /**
+     * @event error
+     * Fires if an error occured when trying to load the image
+     * @param {Ext.Img} this The Image instance
+     * @param {Ext.EventObject} e The event object
+     */
+    config: {
+        /**
+         * @cfg {String} src The source of this image
+         * @accessor
+         */
+        src: null,
+        /**
+         * @cfg
+         * @inheritdoc
+         */
+        baseCls: 'x-img',
+        /**
+         * @cfg {String} imageCls The CSS class to be used when {@link #mode} is not set to 'background'
+         * @accessor
+         */
+        imageCls: 'x-img-image',
+        /**
+         * @cfg {String} backgroundCls The CSS class to be used when {@link #mode} is set to 'background'
+         * @accessor
+         */
+        backgroundCls: 'x-img-background',
+        /**
+         * @cfg {String} mode If set to 'background', uses a background-image CSS property instead of an
+         * `<img>` tag to display the image.
+         */
+        mode: 'background'
+    },
+    beforeInitialize: function() {
+        var me = this;
+        me.onLoad = Ext.Function.bind(me.onLoad, me);
+        me.onError = Ext.Function.bind(me.onError, me);
+    },
+    initialize: function() {
+        var me = this;
+        Ext.Component.prototype.initialize.call(this);
+        me.relayEvents(me.renderElement, '*');
+        me.element.on({
+            tap: 'onTap',
+            scope: me
+        });
+    },
+    hide: function() {
+        Ext.Component.prototype.hide.apply(this, arguments);
+        this.hiddenSrc = this.hiddenSrc || this.getSrc();
+        this.setSrc(null);
+    },
+    show: function() {
+        Ext.Component.prototype.show.apply(this, arguments);
+        if (this.hiddenSrc) {
+            this.setSrc(this.hiddenSrc);
+            delete this.hiddenSrc;
+        }
+    },
+    updateMode: function(mode) {
+        var me = this,
+            imageCls = me.getImageCls(),
+            backgroundCls = me.getBackgroundCls();
+        if (mode === 'background') {
+            if (me.imageElement) {
+                me.imageElement.destroy();
+                delete me.imageElement;
+                me.updateSrc(me.getSrc());
+            }
+            me.replaceCls(imageCls, backgroundCls);
+        } else {
+            me.imageElement = me.element.createChild({
+                tag: 'img'
+            });
+            me.replaceCls(backgroundCls, imageCls);
+        }
+    },
+    updateImageCls: function(newCls, oldCls) {
+        this.replaceCls(oldCls, newCls);
+    },
+    updateBackgroundCls: function(newCls, oldCls) {
+        this.replaceCls(oldCls, newCls);
+    },
+    onTap: function(e) {
+        this.fireEvent('tap', this, e);
+    },
+    onAfterRender: function() {
+        this.updateSrc(this.getSrc());
+    },
+    /**
+     * @private
+     */
+    updateSrc: function(newSrc) {
+        var me = this,
+            dom;
+        if (me.getMode() === 'background') {
+            dom = this.imageObject || new Image();
+        } else {
+            dom = me.imageElement.dom;
+        }
+        this.imageObject = dom;
+        dom.setAttribute('src', Ext.isString(newSrc) ? newSrc : '');
+        dom.addEventListener('load', me.onLoad, false);
+        dom.addEventListener('error', me.onError, false);
+    },
+    detachListeners: function() {
+        var dom = this.imageObject;
+        if (dom) {
+            dom.removeEventListener('load', this.onLoad, false);
+            dom.removeEventListener('error', this.onError, false);
+        }
+    },
+    onLoad: function(e) {
+        this.detachListeners();
+        if (this.getMode() === 'background') {
+            this.element.dom.style.backgroundImage = 'url("' + this.imageObject.src + '")';
+        }
+        this.fireEvent('load', this, e);
+    },
+    onError: function(e) {
+        this.detachListeners();
+        // Attempt to set the src even though the error event fired.
+        if (this.getMode() === 'background') {
+            this.element.dom.style.backgroundImage = 'url("' + this.imageObject.src + '")';
+        }
+        this.fireEvent('error', this, e);
+    },
+    doSetWidth: function(width) {
+        var sizingElement = (this.getMode() === 'background') ? this.element : this.imageElement;
+        sizingElement.setWidth(width);
+        Ext.Component.prototype.doSetWidth.apply(this, arguments);
+    },
+    doSetHeight: function(height) {
+        var sizingElement = (this.getMode() === 'background') ? this.element : this.imageElement;
+        sizingElement.setHeight(height);
+        Ext.Component.prototype.doSetHeight.apply(this, arguments);
+    },
+    destroy: function() {
+        this.detachListeners();
+        Ext.destroy(this.imageObject, this.imageElement);
+        delete this.imageObject;
+        delete this.imageElement;
+        Ext.Component.prototype.destroy.call(this);
+    }
+}, 0, [
+    "image",
+    "img"
+], [
+    "component",
+    "image",
+    "img"
+], {
+    "component": true,
+    "image": true,
+    "img": true
+}, [
+    "widget.image",
+    "widget.img"
+], 0, [
+    Ext,
+    'Img'
+], 0));
+
+/**
  * A simple class used to mask any {@link Ext.Container}.
  *
  * This should rarely be used directly, instead look at the {@link Ext.Container#masked} configuration.
@@ -66604,7 +66818,7 @@ Ext.define('Ext.picker.Picker', {
 ], 0));
 
 /*
- * File: app/view/Picture2.js
+ * File: app/view/MyImg.js
  *
  * This file was generated by Sencha Architect version 3.2.0.
  * http://www.sencha.com/products/architect/
@@ -66617,36 +66831,28 @@ Ext.define('Ext.picker.Picker', {
  *
  * Do NOT hand edit this file.
  */
-(Ext.cmd.derive('Contact.view.Picture2', Ext.Container, {
+(Ext.cmd.derive('Contact.view.MyImg', Ext.Img, {
     config: {
-        itemId: 'contactpic',
-        margin: '5 5 5 5',
-        style: 'overflow:hidden;border:1px groove #000;background:none',
-        styleHtmlContent: true,
-        autoDestroy: false,
-        layout: 'fit',
-        scrollable: false,
-        tpl: [
-            '',
-            '<img src="{pictureURL}" style="width:100%;height:30%"/>',
-            '\t'
-        ]
+        html: '',
+        src: 'http://appsonmobile.com/locallink/stores/Studio%20Nafisa%20Arts.jpg'
     }
 }, 0, [
-    "picture2"
+    "myimg"
 ], [
     "component",
-    "container",
-    "picture2"
+    "image",
+    "img",
+    "myimg"
 ], {
     "component": true,
-    "container": true,
-    "picture2": true
+    "image": true,
+    "img": true,
+    "myimg": true
 }, [
-    "widget.picture2"
+    "widget.myimg"
 ], 0, [
     Contact.view,
-    'Picture2'
+    'MyImg'
 ], 0));
 
 /*
@@ -66734,10 +66940,6 @@ Ext.define('Ext.picker.Picker', {
                         text: 'Save'
                     }
                 ]
-            },
-            {
-                xtype: 'picture2',
-                height: 128
             },
             {
                 xtype: 'button',
@@ -66874,6 +67076,12 @@ Ext.define('Ext.picker.Picker', {
                 labelWrap: true,
                 name: 'websiteDisplayName',
                 required: true
+            },
+            {
+                xtype: 'myimg',
+                flex: 5,
+                docked: 'top',
+                height: '30%'
             }
         ]
     },
@@ -68168,7 +68376,7 @@ Ext.application({
         'ChangeContactPicForm',
         'contactinfo',
         'panel',
-        'Picture2'
+        'MyImg'
     ],
     controllers: [
         'Contacts'
