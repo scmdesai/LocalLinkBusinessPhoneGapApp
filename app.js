84939aeba1645f14c6b319b9346186b35caab4a1
scmdesai <scmdesai@gmail.com>
2016-04-28 16:32:53 -0500

    deal edit

--- app.js
+++ app.js
@@ -68585,7 +68585,7 @@ Ext.define('Ext.picker.Picker', {
                         handler: function(button, e) {
                             var uForm = this.up('UpdateDealForm');
                             var dealName = uForm.getAt(0).getValue();
-                            var itemName = uForm.getAt(7).getValue();
+                            var itemName = uForm.getAt(5).getValue();
                             //var dealStartDate = uForm.getAt(2).getValue().toDateString();
                             //var dealEndDate = uForm.getAt(3).getValue();
                             //Ext.Date.format(uForm.getAt(2).getValue(),'n/j/Y');
