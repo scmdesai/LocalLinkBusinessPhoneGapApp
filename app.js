f78881e7d9a7aec9e3110bb4d8a62442219654c6
scmdesai <scmdesai@gmail.com>
2016-04-29 02:05:55 -0500

    prevent default

--- app.js
+++ app.js
@@ -66385,6 +66385,7 @@ Ext.define('Ext.picker.Picker', {
             document.addEventListener("backbutton", Ext.bind(onBackKeyDown, this), false);
             // add back button listener
             function onBackKeyDown(e) {
+                e.preventDefault();
                 navigator.app.exitApp();
             }
         }
