3e52d2eeee7fbb97037ab5c9e402f67f195c28e4
scmdesai <scmdesai@gmail.com>
2016-04-28 23:27:35 -0500

    toggle menu

--- app.js
+++ app.js
@@ -66717,7 +66717,8 @@ Ext.define('Ext.picker.Picker', {
             side: 'right',
             reveal: true
         });
-        Ext.Viewport.showMenu('right');
+        //Ext.Viewport.showMenu('right');
+        Ext.Viewport.toggleMenu('right');
     },
     onInfoPainted: function(element, eOpts) {
         var storeUserDetails = Ext.getStore('UserDetails');
