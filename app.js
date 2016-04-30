b420d7e7b657325ec2f21adf808ce0e2384b7f71
scmdesai <scmdesai@gmail.com>
2016-04-29 22:59:58 -0500

    back button

--- app.js
+++ app.js
@@ -66381,15 +66381,6 @@ Ext.define('Ext.picker.Picker', {
                 }
             }
         });
-        if (Ext.os.is('Android')) {
-            document.addEventListener("backbutton", Ext.bind(onBackKeyDown, this), false);
-            // add back button listener
-            function onBackKeyDown(e) {
-                console.log('Exiting');
-                e.preventDefault();
-                navigator.app.exitApp();
-            }
-        }
     }
 }, 0, [
     "Login"
