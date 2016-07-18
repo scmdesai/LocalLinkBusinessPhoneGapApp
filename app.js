449fb45f36aeea0fabd1f467b32fb17129d425d1
scmdesai <scmdesai@gmail.com>
2016-07-18 00:18:06 -0500

    viewport

--- app.js
+++ app.js
@@ -67624,7 +67624,7 @@ Ext.define('Ext.picker.Picker', {
                                             xtype: 'DealImage'
                                         });
                                     view.setRecord(record);
-                                    Ext.Viewport.setActiveItem(view);
+                                    view.showBy(Ext.get('dealpPicture'));
                                 });
                             }
                         },
