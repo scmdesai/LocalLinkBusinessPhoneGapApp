532ee66dfdc4d28e73c2601ca55a7396c41c3b35
scmdesai <scmdesai@gmail.com>
2016-07-22 12:53:15 -0500

    back button

--- app.js
+++ app.js
@@ -69737,6 +69737,16 @@ Ext.application({
             function onBackKeyDown(e) {
                 console.log('Back Button pressed');
                 console.log(Ext.Viewport.getActiveItem().xtype);
+                navigator.notification.confirm('Are you certain you want to close the app?', // message
+                function(index) {
+                    if (index == 1) {
+                        //look at the docs for this part
+                        navigator.app.exitApp();
+                    }
+                }, // callback to invoke with index of button pressed
+                'Exit', // title
+                'Yes,No');
+                // buttonLabels
                 if (Ext.Viewport.getActiveItem().xtype === 'panel') {
                     /* if (exitApp) {
 
