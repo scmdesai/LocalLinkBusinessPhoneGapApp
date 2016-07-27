1d3559da3d5be2578d9898abc70286bf7dee8370
scmdesai <scmdesai@gmail.com>
2016-07-27 00:33:51 -0500

    changed urls to demo stores and deals

--- app.js
+++ app.js
@@ -66922,7 +66922,7 @@ Ext.define('Ext.picker.Picker', {
                             store.sync();
                             store.load();
                             form.submit({
-                                url: 'http://services.appsonmobile.com/updateStoreInfo/' + customerId,
+                                url: 'http://services.appsonmobile.com/demoUpdateStoreInfo/' + customerId,
                                 success: function(form, action) {
                                     Ext.Msg.alert('Success', action.msg);
                                     form.destroy();
@@ -67298,7 +67298,7 @@ Ext.define('Ext.picker.Picker', {
                         var itemName = record.get('itemName');
                         var req = Ext.Ajax.request({
                                 method: 'POST',
-                                url: 'http://services.appsonmobile.com/deals/' + itemName,
+                                url: 'http://services.appsonmobile.com/demodeals/' + itemName,
                                 success: function(form, action) {
                                     Ext.Msg.alert('Success', action.msg);
                                     //console.log(action.msg);
@@ -67941,7 +67941,7 @@ Ext.define('Ext.picker.Picker', {
                     var file = form.getAt(0).getValue();
                     if (file) {
                         form.submit({
-                            url: 'http://services.appsonmobile.com/stores/' + customerId,
+                            url: 'http://services.appsonmobile.com/demoStores/' + customerId,
                             xhr2: true,
                             cache: false,
                             waitMsg: 'Please Wait...',
@@ -68572,7 +68572,7 @@ Ext.define('Ext.picker.Picker', {
                                 if (endDate >= date) {
                                     if (document.getElementById('chkbx').checked) {
                                         form.submit({
-                                            url: 'http://services.appsonmobile.com/deals/editDeal/' + itemName,
+                                            url: 'http://services.appsonmobile.com/demodeals/editDeal/' + itemName,
                                             success: function(form, action) {
                                                 Ext.Msg.alert('Success', action.msg);
                                                 form.destroy();
@@ -68912,7 +68912,7 @@ Ext.define('Ext.picker.Picker', {
                                 if (endDate >= date) {
                                     if (document.getElementById('chkbx').checked) {
                                         form.submit({
-                                            url: 'http://services.appsonmobile.com/createNewDeal',
+                                            url: 'http://services.appsonmobile.com/democreateNewDeal',
                                             success: function(form, action) {
                                                 Ext.getStore('MyDealsStore').load();
                                                 Ext.Msg.alert('Success!', action.msg);
@@ -69266,7 +69266,7 @@ Ext.define('Ext.picker.Picker', {
                                     if (endDate >= date) {
                                         if (document.getElementById('chkbx').checked) {
                                             form.submit({
-                                                url: 'http://services.appsonmobile.com/uploadS3/',
+                                                url: 'http://services.appsonmobile.com/demouploadS3/',
                                                 xhr2: true,
                                                 cache: false,
                                                 waitMsg: 'Please Wait...',
