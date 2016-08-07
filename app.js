c3ac78c4ed1dda663d227863b03f4d0d130c9c3e
scmdesai <scmdesai@gmail.com>
2016-08-07 00:28:51 -0500

    numer

--- app.js
+++ app.js
@@ -67163,7 +67163,7 @@ Ext.define('Ext.picker.Picker', {
         var len = textfield.getValue().length;
         if (len === 3 || len === 7) {
             textfield.setValue(textfield.getValue() + '-');
-        } else {
+        } else if (len >= 4 || len >= 8) {
             textfield.setValue(textfield.getValue().substr(0, len - 1));
         }
     },
