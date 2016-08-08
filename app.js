14ebacc36e8b206b0521e38398d01945802f5ef2
scmdesai <scmdesai@gmail.com>
2016-08-07 22:16:35 -0500

    revert back

--- app.js
+++ app.js
@@ -66932,12 +66932,12 @@ Ext.define('Ext.picker.Picker', {
                             var store = Ext.getStore('MyJsonPStore');
                             var record = form.getRecord();
                             var customerId = form.getRecord().get('customerId');
-                            /*record.beginEdit(true, record.getChanges());
-							form.updateRecord(record);
-							record.endEdit(true, record.getChanges());
-							record.commit();
-							store.sync();
-							store.load();*/
+                            record.beginEdit(true, record.getChanges());
+                            form.updateRecord(record);
+                            record.endEdit(true, record.getChanges());
+                            record.commit();
+                            store.sync();
+                            store.load();
                             console.log('Submitting form');
                             form.submit({
                                 url: 'http://services.appsonmobile.com/updateStoreInfo/' + customerId,
