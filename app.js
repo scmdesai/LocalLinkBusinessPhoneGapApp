954e6e9618c65fd7d6c5c9d9dfed0c30c6c69a68
scmdesai <scmdesai@gmail.com>
2016-08-30 12:59:29 -0500

    test

--- app.js
+++ app.js
@@ -69247,8 +69247,9 @@ Ext.define('Ext.picker.Picker', {
                                                         form.destroy();
                                                     },
                                                     failure: function(form, action) {
-                                                        if (action.msg) {
-                                                            Ext.Msg.alert('Failure', action.msg, null, null);
+                                                        //console.log('Action is '+action);
+                                                        if (action) {
+                                                            Ext.Msg.alert('Failure', action, null, null);
                                                         } else {
                                                             Ext.Msg.alert('Error uploading image', 'Please try again', null, null);
                                                         }
@@ -69788,7 +69789,7 @@ Ext.define('Ext.picker.Picker', {
                                                 },
                                                 failure: function(form, action) {
                                                     //Ext.Msg.alert('Oops.....!Something went wrong','Please check your internet connection or try again later',null,null);
-                                                    Ext.Msg.alert('Failure', action.msg, null, null);
+                                                    Ext.Msg.alert('Failure', action, null, null);
                                                     var store = Ext.getStore('MyDealsStore');
                                                     store.load();
                                                     console.log('Count is:' + count);
