3095bee8c372b7b0f59d48b1c2c3b965ce3cccfa
scmdesai <scmdesai@gmail.com>
2016-08-30 10:16:07 -0500

    pass param on load

--- app.js
+++ app.js
@@ -66189,6 +66189,7 @@ Ext.define('Ext.picker.Picker', {
 (Ext.cmd.derive('LocalBuzzMerchant.store.MyDealsStore', Ext.data.Store, {
     config: {
         model: 'LocalBuzzMerchant.model.Deal',
+        remoteFilter: true,
         storeId: 'MyDealsStore',
         proxy: {
             type: 'jsonp',
@@ -68044,8 +68045,9 @@ Ext.define('Ext.picker.Picker', {
                                     'phoneNumber': record.phoneNumber
                                 });
                                 var dealStore = Ext.getStore('MyDealsStore');
-                                dealStore.filter('customerId', customerId);
-                                dealStore.load();
+                                dealStore.load({
+                                    'customerId': customerId
+                                });
                                 var view = Ext.Viewport.add({
                                         xtype: 'panel'
                                     });
