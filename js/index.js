bb56f58fb2aac3b82cb0f9881020474332674922
scmdesai <scmdesai@gmail.com>
2017-03-10 13:10:22 -0600

    deviceready

--- js/index.js
+++ js/index.js
@@ -28,7 +28,7 @@ var app = {
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
          //document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-		 document.addEventListener('deviceready', onDeviceReady, false);
+		 document.addEventListener('deviceready', this.onDeviceReady, false);
 		
     },
     // deviceready Event Handler
