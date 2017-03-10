d69f1f998e03e42ca3351755d5db5c9098393c3b
scmdesai <scmdesai@gmail.com>
2017-03-10 13:41:08 -0600

    index

--- js/index.js
+++ js/index.js
@@ -33,7 +33,7 @@ var app = {
     // The scope of 'this' is the event. In order to call the 'receivedEvent'
     // function, we must explicitly call 'app.receivedEvent(...);'
     onDeviceReady: function() {
-	var push = PushNotification.init({
+	var push = window.PushNotification.init({
             "android": {
                 "senderID": "226322216862"
             },
