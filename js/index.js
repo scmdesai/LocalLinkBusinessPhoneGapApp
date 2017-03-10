1832489088e9f353c417932f482e32114bcecb51
scmdesai <scmdesai@gmail.com>
2017-03-10 14:34:47 -0600

    test if deviceready is triggered

--- js/index.js
+++ js/index.js
@@ -26,8 +26,8 @@ var app = {
     // Bind any events that are required on startup. Common events are:
     // 'load', 'deviceready', 'offline', and 'online'.
     bindEvents: function() {
-	     window.alert('Phonegap ready');
-        document.addEventListener('deviceready', this.onDeviceReady, false);
+	     
+        //document.addEventListener('deviceready', this.onDeviceReady, false);
     },
     // deviceready Event Handler
     //
